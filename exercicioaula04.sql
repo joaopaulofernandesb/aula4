4303949f53c1326e9d99e67231147b4600de101a
João Paulo Fernandes Brasil <joaopaulofernandesbrasil.97@gmail.com>
2019-03-18 09:00:46 -0300

    Exerciico Banco de Dados

---
+++ exercicioaula04.sql
@@ -0,0 +1,179 @@
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cidade`
+--
+
+CREATE TABLE `cidade` (
+  `id_cidade` int(11) NOT NULL,
+  `nome` varchar(90) NOT NULL,
+  `uf` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nome` varchar(90) NOT NULL,
+  `cpf` int(11) NOT NULL,
+  `id_endereco` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `endereco`
+--
+
+CREATE TABLE `endereco` (
+  `id_endereco` int(11) NOT NULL,
+  `rua` varchar(90) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `complemetno` varchar(90) NOT NULL,
+  `cep` varchar(90) NOT NULL,
+  `id_cidade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `recibo`
+--
+
+CREATE TABLE `recibo` (
+  `id_recibo` int(11) NOT NULL,
+  `sequencia` int(11) NOT NULL,
+  `valor` double NOT NULL,
+  `data` date NOT NULL,
+  `assinatura` varchar(90) NOT NULL,
+  `referente` varchar(90) NOT NULL,
+  `id_cidade` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nome` varchar(90) NOT NULL,
+  `login` varchar(90) NOT NULL,
+  `senha` varchar(90) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `id_endereco` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cidade`
+--
+ALTER TABLE `cidade`
+  ADD PRIMARY KEY (`id_cidade`);
+
+--
+-- Indexes for table `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD KEY `id_endereco` (`id_endereco`);
+
+--
+-- Indexes for table `endereco`
+--
+ALTER TABLE `endereco`
+  ADD PRIMARY KEY (`id_endereco`),
+  ADD KEY `id_cidade` (`id_cidade`);
+
+--
+-- Indexes for table `recibo`
+--
+ALTER TABLE `recibo`
+  ADD PRIMARY KEY (`id_recibo`),
+  ADD KEY `id_cidade` (`id_cidade`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `id_endereco` (`id_endereco`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cidade`
+--
+ALTER TABLE `cidade`
+  MODIFY `id_cidade` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `endereco`
+--
+ALTER TABLE `endereco`
+  MODIFY `id_endereco` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `recibo`
+--
+ALTER TABLE `recibo`
+  MODIFY `id_recibo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`id_endereco`) REFERENCES `cliente` (`id_cliente`);
+
+--
+-- Limitadores para a tabela `endereco`
+--
+ALTER TABLE `endereco`
+  ADD CONSTRAINT `endereco_ibfk_1` FOREIGN KEY (`id_cidade`) REFERENCES `endereco` (`id_endereco`);
+
+--
+-- Limitadores para a tabela `recibo`
+--
+ALTER TABLE `recibo`
+  ADD CONSTRAINT `recibo_ibfk_1` FOREIGN KEY (`id_cidade`) REFERENCES `recibo` (`id_recibo`),
+  ADD CONSTRAINT `recibo_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `recibo` (`id_recibo`),
+  ADD CONSTRAINT `recibo_ibfk_3` FOREIGN KEY (`id_cliente`) REFERENCES `recibo` (`id_recibo`);
+
+--
+-- Limitadores para a tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_endereco`) REFERENCES `usuario` (`id_usuario`);
+COMMIT;
+
